c144b8f91e51c3978c335e02202eb723524a2ad4
Ting-Wu0701 <72439203+Ting-Wu0701@users.noreply.github.com>
2022-04-19 15:01:08 +0800

    Update test_log.py

--- test_log.py
+++ test_log.py
@@ -1,11 +1,12 @@
 import time
 
 N = 2000000
-
+print("start!!!")
 for idx in range(N):
     tt = time.localtime(time.time())
     # print("{}".format(idx))
     print("{},{}".format(idx, tt))
+print("end!!!")
 
     # if idx % 5 == 0:
     	# time.sleep(1)
